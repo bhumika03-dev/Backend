d927aced82cc84205ba00fbb8b8a60a9ea3c6e75
bhumika03-dev <ladibhumika03@gmail.com>
2026-07-15 23:05:49 +0530

    Convert Day-11 from submodule to folder

---
+++ Day-11/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const app= require("./src/app.js")
+const connectToDB=require("./src/config/database.js")
+
+
+connectToDB();
+
+app.listen(3000,(req,res)=>{
+    console.log("Server is running in the port 3000")
+})
+
