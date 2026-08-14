e88f5abb81877138446610bb3cd8f51ee6d90df1
bhumika03-dev <ladibhumika03@gmail.com>
2026-08-14 22:17:05 +0530

    Created context and hooks layer for post

--- Day12/Frontend/src/features/posts/pages/Feed.jsx
+++ Day12/Frontend/src/features/posts/pages/Feed.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import '../style/feed.scss'
 import '../components/Post'
+import { usePost } from '../hooks/usePost'
 const Feed=()=>{
+  const {feed,handleGetFeed,loading} = usePost()
+   useEffect(()=>{
+     handleGetFeed()
+   },[])
+   if(loading || !feed){
+    return(<main><h1>Feed is loading...</h1></main>)
+    
+   }
+   console.log(feed)
+
+
     return(
        <main className='feedpage'>
         <div className='feed'>
           <div className="posts">
-            <Post/>
+            {feed.map(post=>{
+              return <Post user={post.user} post={post}/>
+            })}
           </div>
         </div>
        </main>
